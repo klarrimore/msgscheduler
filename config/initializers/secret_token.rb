b04cbd2f3a44978fed8e7b9e80213cb19e4b871b
Keith Larrimore <keithlarrimore@gmail.com>
2013-02-22 10:48:30 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Msgscheduler::Application.config.secret_token = 'bd3bd94f6fea746f68408014dae0644894c3a5531fae396c1f4b4efee6d4e0aee6a4325396e9ae8435f0e941765c5fafa4f86c74d8ce2e721368868e440c37f8'
